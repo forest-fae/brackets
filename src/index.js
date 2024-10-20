b02a56f31ac64cbfd98ae47b598f3ba0394b664b
forest-fae <surreal.autumn@gmail.com>
2024-10-20 19:41:38 +0500

    fix: change solution to cover all test cases

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ module.exports =
 
 function check(str, bracketsConfig) {
   let objectBrackets = bracketsConfig .reduce((acc, [key, value])=>({...acc, [value]: key}), {});
-  console.log(objectBrackets);
- 
   let openBrackets = Object.values(objectBrackets);
-  console.log(openBrackets);
- 
   let stack = [];
  
      if (str.length % 2 !== 0) {
@@ -14,92 +10,25 @@ function check(str, bracketsConfig) {
      };
  
   for (let i = 0; i < str.length; i++) {
+
    let current = str[i];
-   if (openBrackets.includes(current)) {
-     stack.push(current);
-       console.log(stack);
-       if (current === '|' && stack[stack.length - 1] === '|') {
-           stack.pop()
-       }
-     } else {
- 
-       if (stack.length === 0) {
-           return false;
-       }
- 
-     let top = stack[stack.length - 1];
-     if(objectBrackets[current] === top) {
-         console.log(objectBrackets[current]);
-       stack.pop();
-         console.log(stack);
-     }
-   }
-     
-  } 
+   let top = stack[stack.length - 1];
 
-     return stack.length === 0;
+   if (current === objectBrackets[current] && current === top ) {
+    stack.pop();
+  } else if (openBrackets.includes(current)) {
+    stack.push(current);
+  } else if (objectBrackets[current] === top) {
+    stack.pop();
+  } else {
+    return false;
+  }
 }
-    
-   /*   const allEqual = arr => arr.every(val => val === arr[0]);
-      const result = allEqual(stack);
-         console.log(result);
-     if (result === true && stack.length % 2 === 0) {
-         return true
-     } else {
-         return false
-        }
+return stack.length === 0;
 }
-        */
+
+
  
 
 
 
-
-/*function check(str, bracketsConfig) {
-  let objectBrackets = bracketsConfig .reduce((acc, [key, value])=>({...acc, [value]: key}), {});
-  console.log(objectBrackets);
- 
-  let openBrackets = Object.values(objectBrackets);
-  console.log(openBrackets);
- 
-  let stack = [];
- 
-     if (str.length % 2 !== 0) {
-        return false
-     };
- 
-  for (let i = 0; i < str.length; i++) {
-   let current = str[i];
-   if (openBrackets.includes(current)) {
-     stack.push(current);
-       console.log(stack);
-     } else {
- 
-       if (stack.length === 0) {
-           return false;
-       }
- 
-     let top = stack[stack.length - 1];
-     if(objectBrackets[current] === top) {
-         console.log(objectBrackets[current]);
-       stack.pop();
-         console.log(stack);
-     } else {
-       return false
-     }  
-   }
-  } 
-      /* const allEqual = arr => arr.every(val => val === arr[0]);
-      const result = allEqual(stack);
-         console.log(result);
-     if (result === true && stack.length % 2 === 0) {
-         return true
-     } else {
-         return false
-        }
-        
- } */
-
-
-//check('|()|', [['(', ')'], ['|', '|']]) // -> true
-//check('|(|)', [['(', ')'], ['|', '|']]) // -> false */
